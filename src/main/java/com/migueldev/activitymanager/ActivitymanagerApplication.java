020eba63872c3e82e83976c6f0f97d8252242751
Miguel Francischini <migueldev29@gmail.com>
2024-03-26 11:49:41 -0300

    Projeto criado

---
+++ src/main/java/com/migueldev/activitymanager/ActivitymanagerApplication.java
@@ -0,0 +1,13 @@
+package com.migueldev.activitymanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivitymanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivitymanagerApplication.class, args);
+	}
+
+}
